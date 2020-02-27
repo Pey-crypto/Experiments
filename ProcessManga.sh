84dd9903092a5cbca8875baf547c5c3e62f878ce
Pey-crypto <53893419+Pey-crypto@users.noreply.github.com>
2020-02-27 21:40:15 +0530

    jUST mAGIC

--- ProcessManga.sh
+++ ProcessManga.sh
@@ -1,32 +1,29 @@
-#!/bin/sh
+
+!/bin/sh
 rm process.txt
 echo "============================="
-sleep 2s
+
 echo "Procces Killer"
 echo "============================="
-sleep 2s
+
 echo "Running the cmd , edit lines if required "
 echo ""
 echo "File creation"
 echo ""
-##echo -ne '#####                     (33%)\r'
-##sleep 1
-##echo -ne '#############             (66%)\r'
-##sleep 1
-##echo -ne '#######################   (100%)\r'
-##echo -ne '\n'
-ps -eo %cpu,%mem,pid,command | sort -r | head  >> process.txt
-sleep 2s
+ps -eo %cpu,%mem,pid,command | sort -nrk 1 | head  >> process.txt
+cat process.txt
 echo " Enter the Threshold "
 read n4
 while IFS= read -r line  
 do 
-    a= echo $line | awk '{print $1}'
-    b= echo $line | awk '{print $3}'
+    a=`echo $line | awk '{print $1}'`
+    b=`echo $line | awk '{print $3}'`
+    c=`echo $line | awk '{print $4}'`
     if [ $a > $n4 ]
      then 
-        echo "h"$a
-        echo "l"$b
-        kill $b
+        echo "Command :" $c
+        echo "Process id :"$b
+        kill -9 $b
+        sleep 2s
     fi
 done < process.txt
